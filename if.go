0b2f885c5ab617bf3c9af68898398cc2121754e6
Rido Mulanda <ridomulanda@gmail.com>
2021-05-21 13:54:18 +0700

    If else statement

--- if.go
+++ if.go
@@ -3,11 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	var name = "Hello World"
+	var name = "Hello All"
 
 	if name == "Hello" {
-		fmt.Println("Hello World")
+		fmt.Println("Hello")
+	} else if name == "World" {
+		fmt.Println("World")
 	} else {
 		fmt.Println("No")
+	}
+
+	// Shor statement
+	if length := len(name); length > 5 {
+		fmt.Println("Too Long!")
+	} else {
+		fmt.Println("Yeay, Correct")
 	}
 }
