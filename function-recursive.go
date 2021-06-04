b995bcf33f4d598690529339901f8d857792b527
Rido Mulanda <ridomulanda@gmail.com>
2021-06-04 13:18:35 +0700

    Function Recursive

---
+++ function-recursive.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func factorialLoop(value int) int {
+	result := 1
+
+	for i := value; i > 0; i-- {
+		result *= i
+	}
+
+	return result
+}
+
+func factorialRecursive(value int) int {
+	if value == 1 {
+		return 1
+	} else {
+		return value * factorialRecursive(value-1)
+	}
+}
+
+func main() {
+	loop := factorialLoop(18)
+	fmt.Println(loop)
+
+	recursive := factorialRecursive(18)
+	fmt.Println(recursive)
+
+}
